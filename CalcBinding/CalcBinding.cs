4cd3a6ec824cfc38c39a6f388f57421b1456688f
AlexandrZinchenko <sasha-zin@mail.ru>
2014-12-03 00:36:17 +0300

    standart binding properties added Path removed from xaml singleValue converter added two way binding for value and !value added (it is singleValue converter)

--- CalcBinding/CalcBinding.cs
+++ CalcBinding/CalcBinding.cs
@@ -12,15 +12,23 @@ using System.Globalization;
 
 using OldBinding = System.Windows.Data.Binding;
 
+using System.ComponentModel;
+
 namespace CalcBinding
 {
     public class Binding : MarkupExtension
     {
+        //[ConstructorArgument]
         public String Path { get; set; }
 
         public Binding()
         {
 
+        }
+
+        public Binding(String path)
+        {
+            Path = path;
         }
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
@@ -36,13 +44,14 @@ namespace CalcBinding
             replaceDict.Add("and", "&&");
             replaceDict.Add("or", "||");
             replaceDict.Add("less=", "<=");
+            replaceDict.Add(" ", "");
 
             var normPath = Path;
             foreach (var pair in replaceDict)
                 normPath = normPath.Replace(pair.Key, pair.Value);
 
             // это уже получить данные
-            var matches = Regex.Matches(normPath.Replace(" ", ""), @"[a-zA-Z]+(\.[a-zA-Z]+)*");
+            var matches = Regex.Matches(normPath, @"[a-zA-Z]+(\.[a-zA-Z]+)*");
             var pathsList = new List<String>();
             foreach (var match in matches.OfType<Match>())
             {
@@ -50,20 +59,87 @@ namespace CalcBinding
             }
             pathsList = pathsList.Distinct().ToList();
 
-            var mathPath = normPath; 
+            var exprTemplate = normPath; 
             
             foreach (var p in pathsList)
-                mathPath = mathPath.Replace(p, pathsList.IndexOf(p).ToString("{0}"));
+                exprTemplate = exprTemplate.Replace(p, pathsList.IndexOf(p).ToString("{0}"));
 
-            var mBinding = new MultiBinding();
             var mathConverter = new CalcConverter();
-            mBinding.Converter = mathConverter;
-            mBinding.ConverterParameter = mathPath;
 
-            foreach (var path in pathsList)
+            BindingBase resBinding;
+            //определяем возможность mode=twoway. Из коробки это только 1 переменная и 
+            // либо она сама либо отрицание от неё
+
+            if (pathsList.Count == 1)
             {
-                var binding = new OldBinding(path);
-                mBinding.Bindings.Add(binding);
+                var binding = new OldBinding(pathsList.First())
+                {
+                    Mode = Mode,
+                    NotifyOnSourceUpdated = NotifyOnSourceUpdated,
+                    NotifyOnTargetUpdated = NotifyOnTargetUpdated,
+                    NotifyOnValidationError = NotifyOnValidationError,
+                    UpdateSourceExceptionFilter = UpdateSourceExceptionFilter,
+                    UpdateSourceTrigger = UpdateSourceTrigger,
+                    ValidatesOnDataErrors = ValidatesOnDataErrors,
+                    ValidatesOnExceptions = ValidatesOnExceptions,
+                    ValidatesOnNotifyDataErrors = ValidatesOnNotifyDataErrors,
+                    RelativeSource = RelativeSource
+                };
+
+                if (Source != null)
+                    binding.Source = Source;
+
+                if (ElementName != null)
+                    binding.ElementName = ElementName;
+
+                if (StringFormat != null)
+                    binding.StringFormat = StringFormat;
+
+                if (exprTemplate != "{0}")
+                {
+                    binding.Converter = mathConverter;
+                    binding.ConverterParameter = exprTemplate;
+                    binding.ConverterCulture = ConverterCulture;
+
+                }
+                resBinding = binding;
+            }
+            else
+            {
+                var mBinding = new MultiBinding
+                {
+                    Converter = mathConverter,
+                    ConverterParameter = exprTemplate,
+                    ConverterCulture = ConverterCulture,
+                    Mode = BindingMode.OneWay,
+                    NotifyOnSourceUpdated = NotifyOnSourceUpdated,
+                    NotifyOnTargetUpdated = NotifyOnTargetUpdated,
+                    NotifyOnValidationError = NotifyOnValidationError,
+                    UpdateSourceExceptionFilter = UpdateSourceExceptionFilter,
+                    UpdateSourceTrigger = UpdateSourceTrigger,
+                    ValidatesOnDataErrors = ValidatesOnDataErrors,
+                    ValidatesOnExceptions = ValidatesOnExceptions,
+                    ValidatesOnNotifyDataErrors = ValidatesOnNotifyDataErrors,
+                };
+
+                if (StringFormat != null)
+                    mBinding.StringFormat = StringFormat;
+
+                foreach (var path in pathsList)
+                {
+                    var binding = new OldBinding(path);
+
+                    if (Source != null)
+                        binding.Source = Source;
+
+                    if (ElementName != null)
+                        binding.ElementName = ElementName;
+
+                    mBinding.Bindings.Add(binding);
+
+                }
+
+                resBinding = mBinding;
             }
 
             //Получим провайдер, с информацией об объекте и привязках
@@ -76,21 +152,191 @@ namespace CalcBinding
             //Получим свойство для привязки
             DependencyProperty _targetProperty = (DependencyProperty)providerValuetarget.TargetProperty; 
             
-            //_targetObject.GetType().GetProperty(Path);
-
-            // прикрепим биндинг
-            //_targetObject.SetBinding(_targetProperty, mBinding);
-
-            ////Вернем значение свойства
-            //var context = _targetObject.DataContext;
-            //var contextType = context.GetType();
-            //var curValues = pathsList.Select(p => contextType.GetProperty(p).GetValue(context)).ToList();
-            //var defaultValue = mathConverter.Convert(curValues, _targetProperty.PropertyType, mathPath, CultureInfo.CurrentCulture);
-            //// не знаю, что возвращать
-            //return defaultValue;//new MultiBindingExpression();
-            //return _sourceProperty.GetValue(_targetObject, null);
-
-            return mBinding.ProvideValue(serviceProvider);
+            return resBinding.ProvideValue(serviceProvider);
         }
+
+
+        #region Binding Properties
+		        //
+        // Summary:
+        //     Gets or sets the converter to use to convert the source values to or from
+        //     the target value.
+        //
+        // Returns:
+        //     A value of type System.Windows.Data.IMultiValueConverter that indicates the
+        //     converter to use. The default value is null.
+        [DefaultValue("")]
+        public IMultiValueConverter Converter { get; set; }
+        //
+        // Summary:
+        //     Gets or sets the System.Globalization.CultureInfo object that applies to
+        //     any converter assigned to bindings wrapped by the System.Windows.Data.MultiBinding
+        //     or on the System.Windows.Data.MultiBinding itself.
+        //
+        // Returns:
+        //     A valid System.Globalization.CultureInfo.
+        [DefaultValue("")]
+        [TypeConverter(typeof(CultureInfoIetfLanguageTagConverter))]
+        public CultureInfo ConverterCulture { get; set; }
+        //
+        // Summary:
+        //     Gets or sets an optional parameter to pass to a converter as additional information.
+        //
+        // Returns:
+        //     A parameter to pass to a converter. The default value is null.
+        [DefaultValue("")]
+        public object ConverterParameter { get; set; }
+        //
+        // Summary:
+        //     Gets or sets a value that indicates the direction of the data flow of this
+        //     binding.
+        //
+        // Returns:
+        //     One of the System.Windows.Data.BindingMode values. The default value is System.Windows.Data.BindingMode.Default,
+        //     which returns the default binding mode value of the target dependency property.
+        //     However, the default value varies for each dependency property. In general,
+        //     user-editable control properties, such as System.Windows.Controls.TextBox.Text,
+        //     default to two-way bindings, whereas most other properties default to one-way
+        //     bindings.A programmatic way to determine whether a dependency property binds
+        //     one-way or two-way by default is to get the property metadata of the property
+        //     using System.Windows.DependencyProperty.GetMetadata(System.Type) and then
+        //     check the Boolean value of the System.Windows.FrameworkPropertyMetadata.BindsTwoWayByDefault
+        //     property.
+        public BindingMode Mode { get; set; }
+        //
+        // Summary:
+        //     Gets or sets a value that indicates whether to raise the System.Windows.FrameworkElement.SourceUpdated
+        //     event when a value is transferred from the binding target to the binding
+        //     source.
+        //
+        // Returns:
+        //     true if the System.Windows.FrameworkElement.SourceUpdated event will be raised
+        //     when the binding source value is updated; otherwise, false. The default value
+        //     is false.
+        [DefaultValue(false)]
+        public bool NotifyOnSourceUpdated { get; set; }
+        //
+        // Summary:
+        //     Gets or sets a value that indicates whether to raise the System.Windows.FrameworkElement.TargetUpdated
+        //     event when a value is transferred from the binding source to the binding
+        //     target.
+        //
+        // Returns:
+        //     true if the System.Windows.FrameworkElement.TargetUpdated event will be raised
+        //     when the binding target value is updated; otherwise, false. The default value
+        //     is false.
+        [DefaultValue(false)]
+        public bool NotifyOnTargetUpdated { get; set; }
+        //
+        // Summary:
+        //     Gets or sets a value that indicates whether to raise the System.Windows.Controls.Validation.Error attached
+        //     event on the bound element.
+        //
+        // Returns:
+        //     true if the System.Windows.Controls.Validation.Error attached event will
+        //     be raised on the bound element when there is a validation error during source
+        //     updates; otherwise, false. The default value is false.
+        [DefaultValue(false)]
+        public bool NotifyOnValidationError { get; set; }
+        //
+        // Summary:
+        //     Gets or sets a handler you can use to provide custom logic for handling exceptions
+        //     that the binding engine encounters during the update of the binding source
+        //     value. This is only applicable if you have associated the System.Windows.Controls.ExceptionValidationRule
+        //     with your System.Windows.Data.MultiBinding object.
+        //
+        // Returns:
+        //     A method that provides custom logic for handling exceptions that the binding
+        //     engine encounters during the update of the binding source value.
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public UpdateSourceExceptionFilterCallback UpdateSourceExceptionFilter { get; set; }
+        //
+        // Summary:
+        //     Gets or sets a value that determines the timing of binding source updates.
+        //
+        // Returns:
+        //     One of the System.Windows.Data.UpdateSourceTrigger values. The default value
+        //     is System.Windows.Data.UpdateSourceTrigger.Default, which returns the default
+        //     System.Windows.Data.UpdateSourceTrigger value of the target dependency property.
+        //     However, the default value for most dependency properties is System.Windows.Data.UpdateSourceTrigger.PropertyChanged,
+        //     while the System.Windows.Controls.TextBox.Text property has a default value
+        //     of System.Windows.Data.UpdateSourceTrigger.LostFocus.A programmatic way to
+        //     determine the default System.Windows.Data.Binding.UpdateSourceTrigger value
+        //     of a dependency property is to get the property metadata of the property
+        //     using System.Windows.DependencyProperty.GetMetadata(System.Type) and then
+        //     check the value of the System.Windows.FrameworkPropertyMetadata.DefaultUpdateSourceTrigger
+        //     property.
+        public UpdateSourceTrigger UpdateSourceTrigger { get; set; }
+        //
+        // Summary:
+        //     Gets or sets a value that indicates whether to include the System.Windows.Controls.DataErrorValidationRule.
+        //
+        // Returns:
+        //     true to include the System.Windows.Controls.DataErrorValidationRule; otherwise,
+        //     false.
+        [DefaultValue(false)]
+        public bool ValidatesOnDataErrors { get; set; }
+        
+        // Summary:
+        //     Gets or sets a value that indicates whether to include the System.Windows.Controls.ExceptionValidationRule.
+        //
+        // Returns:
+        //     true to include the System.Windows.Controls.ExceptionValidationRule; otherwise,
+        //     false.
+        [DefaultValue(false)]
+        public bool ValidatesOnExceptions { get; set; }
+ 
+        //
+        // Summary:
+        //     Gets or sets a value that indicates whether to include the System.Windows.Controls.NotifyDataErrorValidationRule.
+        //
+        // Returns:
+        //     true to include the System.Windows.Controls.NotifyDataErrorValidationRule;
+        //     otherwise, false. The default is true.
+        [DefaultValue(true)]
+        public bool ValidatesOnNotifyDataErrors { get; set; }
+
+        //
+        // Summary:
+        //     Gets or sets the binding source by specifying its location relative to the
+        //     position of the binding target.
+        //
+        // Returns:
+        //     A System.Windows.Data.RelativeSource object specifying the relative location
+        //     of the binding source to use. The default is null.
+        [DefaultValue("")]
+        public RelativeSource RelativeSource { get; set; }
+        //
+        // Summary:
+        //     Gets or sets the object to use as the binding source.
+        //
+        // Returns:
+        //     The object to use as the binding source.
+        public object Source { get; set; }
+
+        //
+        // Summary:
+        //     Gets or sets the name of the element to use as the binding source object.
+        //
+        // Returns:
+        //     The value of the Name property or x:Name Directive of the element of interest.
+        //     You can refer to elements in code only if they are registered to the appropriate
+        //     System.Windows.NameScope through RegisterName. For more information, see
+        //     WPF XAML Namescopes.The default is null.
+        [DefaultValue("")]
+        public string ElementName { get; set; }
+
+        //
+        // Summary:
+        //     Gets or sets a string that specifies how to format the binding if it displays
+        //     the bound value as a string.
+        //
+        // Returns:
+        //     A string that specifies how to format the binding if it displays the bound
+        //     value as a string.
+        [DefaultValue("")]
+        public string StringFormat { get; set; }
+
+	    #endregion    
     }
 }
--- CalcBinding/CalcConverter.cs
+++ CalcBinding/CalcConverter.cs
@@ -5,12 +5,43 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace CalcBinding
 {
-    public class CalcConverter : IMultiValueConverter
+    public class CalcConverter : IValueConverter, IMultiValueConverter
     {
+        #region IValueConverter
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(new [] { value }, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var exprTemplate = (String)parameter; 
+
+            if (exprTemplate == "!{0}" && targetType == typeof(bool))
+            {
+                if (value.GetType() == typeof(Visibility))
+                {
+                    value = new BoolToVisibilityConverter(FalseToVisibility)
+                        .ConvertBack(value, targetType, null, culture);
+                }
+
+                if (value.GetType() == typeof(bool))
+                    return !(bool)value;
+            }
+
+            throw new NotSupportedException();
+        } 
+
+        #endregion
+        
+        #region IMultiValueConverter
+        
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             var expressionStr = (String)parameter;
@@ -19,15 +50,83 @@ namespace CalcBinding
                 expressionStr = expressionStr.Replace("{" + i.ToString() + "}", values[i].ToString().ToLower());
             }
 
-            var expression = new Expression(expressionStr);
+            var expression = new NCalc.Expression(expressionStr);
             object result = expression.Evaluate();
 
-            return result.ToString();
+            if (targetType == typeof(Visibility))
+            {
+                result = new BoolToVisibilityConverter(FalseToVisibility)
+                                .Convert(result, targetType, null, culture);
+            }
+
+            if (targetType == typeof(String))
+                result = result.ToString();
+
+            return result;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
+        } 
+
+        #endregion
+
+        public FalseToVisibility FalseToVisibility { get; set; }
+
+        #region Init
+        
+        public CalcConverter()
+        {
+            FalseToVisibility = CalcBinding.FalseToVisibility.Collapsed;
         }
+
+        public CalcConverter(FalseToVisibility falseToVisibility)
+        {
+            FalseToVisibility = falseToVisibility;
+        } 
+
+        #endregion
+    }
+
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public BoolToVisibilityConverter()
+        {
+            FalseToVisibility = FalseToVisibility.Collapsed;
+        }
+
+        public BoolToVisibilityConverter(FalseToVisibility falseToVisibility)
+        {
+            FalseToVisibility = falseToVisibility;
+        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var b = (bool)value;
+
+            if (b)
+                return Visibility.Visible;
+
+            if (FalseToVisibility == FalseToVisibility.Collapsed)
+                return Visibility.Collapsed;
+
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var visible = (Visibility)value;
+
+            return visible == Visibility.Visible;
+        }
+
+        public FalseToVisibility FalseToVisibility { get; set; }
+
+    }
+
+    public enum FalseToVisibility
+    {
+        Hidden,
+        Collapsed
     }
 }
