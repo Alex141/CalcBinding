e4e56bc668d7db933cbf680da34f23ce12796766
Alex14 <sasha-zin@mail.ru>
2018-02-14 22:42:58 +0300

    add credentials

--- CalcBinding/Properties/AssemblyInfo.cs
+++ CalcBinding/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CalcBinding")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Advanced binding markup extension")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alexander Zinchenko")]
 [assembly: AssemblyProduct("CalcBinding")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Alexander Zinchenko 2014-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
